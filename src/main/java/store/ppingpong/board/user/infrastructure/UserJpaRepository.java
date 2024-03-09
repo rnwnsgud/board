caf55082f2fd95d64565f24a73b50c587f5c6e64
rnwnsgud <rnwnsgud90@naver.com>
2024-03-09 23:37:55 +0900

    [Refactor] @Query 메서드에 @Transactional(readOnly = true) 추가

--- src/main/java/store/ppingpong/board/user/infrastructure/UserJpaRepository.java
+++ src/main/java/store/ppingpong/board/user/infrastructure/UserJpaRepository.java
@@ -3,6 +3,7 @@ package store.ppingpong.board.user.infrastructure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,12 +12,14 @@ public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByLoginInfoLoginId(String loginId);
 
+    @Transactional(readOnly = true)
     @Query("select u " +
             "from ForumManagerEntity fm " +
             "join UserEntity u on fm.userId = u.id " +
             "where fm.forumId = :forumId and fm.forumManagerLevel = 'MANAGER'")
     UserEntity findForumManager(@Param("forumId") String forumId);
 
+    @Transactional(readOnly = true)
     @Query("select u " +
             "from ForumManagerEntity fm " +
             "join UserEntity u on fm.userId = u.id " +
