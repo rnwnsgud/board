9a93aad08f33a02705fc0f0577c497482e7fa9fe
rnwnsgud <rnwnsgud90@naver.com>
2024-05-08 00:41:20 +0900

    [Refactor] 예외처리클래스 생성자 추가

--- src/main/java/store/ppingpong/board/common/handler/exception/ResourceAlreadyExistException.java
+++ src/main/java/store/ppingpong/board/common/handler/exception/ResourceAlreadyExistException.java
@@ -5,4 +5,8 @@ public class ResourceAlreadyExistException extends RuntimeException {
     public ResourceAlreadyExistException(String message, long id) {
         super(message + " id : " + id);
     }
+
+    public ResourceAlreadyExistException(String message) {
+        super(message);
+    }
 }
